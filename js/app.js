9deb563b581f320d3ea503a84ad01420e5be5464
urvashi atodaria <urvashigohil@outlook.com>
2016-03-01 16:27:15 -0500

    Interactive version

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	/*
 	var questions=[{
 		questionNumber:0,
 		question:"Which of the following planets would float if you put it in water?",
@@ -37,65 +38,316 @@ $(document).ready(function(){
 		backgroundImage:"../Images/Mountain.jpg"
 	}];
 
+*/
+
 //  global variables
 
 	var totalCorrect=0;
-	var currentQuestion=0;
+	// var currentQuestion=0;
 
 //  clicking submit button after answering each question
 
-	$('.questionsList').on("click","#submit",function(){
+/*	$('.questionsList').on("click","#submit",function(){
 		updateQuestion();
 		currentQuestion++;
 		feedback();
 	});
+*/
 
 //  restart button
 
-	$('.questionsList').on("click","#restart",function(){
-		currentQuestion=0;
-		totalCorrect=0;
-		var newQuestion="<div class="questionsList">"+questions[currentQuestion].question+"</div><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="0">"+questions[currentQuestion].options[0]+"</div></span><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="1">"+questions[currentQuestion].options[1]+"</div></span><br /><div id="button_holder"><input type="button" id="submit" value="Submit"></div>";
-		$(".questionsList").html(newQuestion);
-		$(".feedback_holder").html("");
+	$('#restart').on("click",function(){
+		location.reload();
+	});
+
+//  start the quiz RETRY!
+
+	$('#start').on('click',function(){
+		// alert('Start the quiz?');
+		console.log("start button functions");
+		$('#start').hide();
+		q1Appear();
+	});
+
+//  q1Appear function
+
+	function q1Appear(){
+		$('.firstQue').fadeIn(100);
+		$('#q1').fadeIn(1000);
+		$('#button_holder').fadeIn(1000);
+		$('body').css('background-image','url(Images/Water.jpg)');
+		$('body').css('color','#424242');
+		$('#correct').hide();
+		$('#incorrect').hide();
+		$('#u1').removeClass('unAnswered');
+		$('#u1').addClass('currentQuestion');
+	}
+
+	function q1Clear(){
+		$('.firstQue').hide();
+		$('#q1').hide();
+		$('#u1').removeClass('currentQuestion');
+		$('#u1').addClass('rightAnswer');
+		$('.ans').removeClass('selectedAnswer');
+	}
+
+
+	function q2Appear(){
+		$('#fact1').hide();
+		$('#next_button_holder').hide();
+		$('.secondQue').fadeIn(100);
+		$('#q2').fadeIn(1000);
+		$('#button_holder').fadeIn(1000);
+		$('body').css('background-image','url(Images/Summer.jpg)');
+		$('body').css('color','#ff3d00');
+		$('#correct').hide();
+		$('#incorrect').hide();
+		$('#u2').removeClass('unAnswered');
+		$('#u2').addClass('currentQuestion');
+	}
+
+	function q2Clear(){
+		$('.secondQue').hide();
+		$('#q2').hide();
+		$('#u2').removeClass('currentQuestion');
+		$('#u2').addClass('rightAnswer');
+		$('.ans').removeClass('selectedAnswer');
+	}
+
+	function q3Appear(){
+		$('#fact2').hide();
+		$('#next_button_holder').hide();
+		$('.thirdQue').fadeIn(100);
+		$('#q3').fadeIn(1000);
+		$('#button_holder').fadeIn(1000);
+		$('body').css('background-image','url(Images/Cold.jpg)');
+		$('body').css('color','black');
+		$('#correct').hide();
+		$('#incorrect').hide();
+		$('#u3').removeClass('unAnswered');
+		$('#u3').addClass('currentQuestion');
+	}
+
+	function q3Clear(){
+		$('.thirdQue').hide();
+		$('#q3').hide();
+		$('#u3').removeClass('currentQuestion');
+		$('#u3').addClass('rightAnswer');
+		$('.ans').removeClass('selectedAnswer');
+	}
+
+	function q4Appear(){
+		$('#fact3').hide();
+		$('#next_button_holder').hide();
+		$('.fourthQue').fadeIn(100);
+		$('#q4').fadeIn(1000);
+		$('#button_holder').fadeIn(1000);
+		$('body').css('background-image','url(Images/Diamonds.jpg)');
+		$('body').css('color','#1a237e');
+		$('#correct').hide();
+		$('#incorrect').hide();
+		$('#u4').removeClass('unAnswered');
+		$('#u4').addClass('currentQuestion');
+	}
+
+	function q4Clear(){
+		$('.fourthQue').hide();
+		$('#q4').hide();
+		$('#u4').removeClass('currentQuestion');
+		$('#u4').addClass('rightAnswer');
+		$('.ans').removeClass('selectedAnswer');
+	}
+
+	function q5Appear(){
+		$('#fact4').hide();
+		$('#next_button_holder').hide();
+		$('.fifthQue').fadeIn(100);
+		$('#q5').fadeIn(1000);
+		$('#button_holder').fadeIn(1000);
+		$('body').css('background-image','url(Images/Mountain.jpg)');
+		$('body').css('color','white');
+		$('#correct').hide();
+		$('#incorrect').hide();
+		$('#u5').removeClass('unAnswered');
+		$('#u5').addClass('currentQuestion');
+	}
+
+	function q5Clear(){
+		$('.fifthQue').hide();
+		$('#q5').hide();
+		$('#u5').removeClass('currentQuestion');
+		$('#u5').addClass('rightAnswer');
+		$('.ans').removeClass('selectedAnswer');
+	}
+
+	function checkAnswer(question,answer){
+		if((question=="q1") && (answer=="Saturn")){
+			q1Clear();
+			$('#correct').css({'display':'inline-block'});
+			$('#fact1').show();
+			$('#u1').addClass('rightAnswer');
+		}
+		else if((question=="q1") && (answer!="Saturn")){
+			q1Clear();
+			$('#incorrect').show();
+			$('#fact1').show();
+			$('#u1').addClass('wrongAnswer');
+		}
+		else if((question=="q2") && (answer=="Venus")){
+			q2Clear();
+			$('#correct').show();
+			$('#correct').css('font-size','1.7rem');
+			$('#fact2').show();
+			$('#fact2').css('font-size','1.7rem');
+			$('#u2').addClass('rightAnswer');
+		}
+		else if((question=="q2") && (answer!="Venus")){
+			q2Clear();
+			$('#incorrect').show();
+			$('#fact2').show();
+			$('#fact2').css('font-size','1.7rem');
+			$('#incorrect').css('font-size','1.7rem');
+			$('#u2').addClass('wrongAnswer');
+		}
+		else if((question=="q3") && (answer=="The Boomerang Nebula")){
+			q3Clear();
+			$('#correct').show();
+			$('#fact3').show();
+			$('#fact3').css('font-size','1.7rem');
+			$('#correct').css('font-size','1.7rem');
+			$('#u3').addClass('rightAnswer');
+		}
+		else if((question=="q3") && (answer!="The Boomerang Nebula")){
+			q3Clear();
+			$('#incorrect').show();
+			$('#fact3').show();
+			$('#fact3').css('font-size','1.7rem');
+			$('#incorrect').css('font-size','1.7rem');
+			$('#u3').addClass('wrongAnswer');
+		}
+		else if((question=="q4") && (answer=="Lucy")){
+			q4Clear();
+			$('#correct').show();
+			$('#fact4').show();
+			$('#correct').css('font-size','1.7rem');
+			$('#fact4').css('font-size','1.7rem');
+			$('#u4').addClass('rightAnswer');
+		}
+		else if((question=="q4") && (answer!="Lucy")){
+			q4Clear();
+			$('#incorrect').show();
+			$('#fact4').show();
+			$('#incorrect').css('font-size','1.7rem');
+			$('#fact4').css('font-size','1.7rem');
+			$('#u4').addClass('wrongAnswer');
+		}
+		else if((question=="q5") && (answer=="Mars")){
+			q5Clear();
+			$('#correct').show();
+			$('#fact5').show();
+			$('#fact5').css('font-size','1.7rem');
+			$('#correct').css('font-size','1.7rem');
+			$('#u5').addClass('rightAnswer');
+		}
+		else if((question=="q5") && (answer!="Mars")){
+			q5Clear();
+			$('#incorrect').show();
+			$('#fact5').show();
+			$('#fact5').css('font-size','1.7rem');
+			$('#incorrect').css('font-size','1.7rem');
+			$('#u5').addClass('wrongAnswer');
+		}
+	}
+
+
+//  clicking submit button
+
+	$('#submit').on('click',function(){
+	    // alert('Submit button pressed');
+		console.dir($('.selectedAnswer')[0]);
+		var questionNumber = $('.selectedAnswer')[0].parentElement.id;
+		var questionAnswer = $('.selectedAnswer')[0].getAttribute("name");
+		checkAnswer(questionNumber, questionAnswer);
+	    $('#button_holder').hide();
+	    $('#next_button_holder').show();
+	});
+
+
+	$('.ans').on('click',function(){
+		console.dir(this);
+		$('.ans').removeClass('selectedAnswer');
+		$(this).addClass('selectedAnswer');
+	});
+
+	$('#next_button_holder').on('click',function(){
+		// alert('Next button pressed');
+		var message="You got " + (5-$('.wrongAnswer').length) + " out 5 quetions correct!";
+
+		$('#correct').hide();
+		$('#incorrect').hide();
+		if($('.unAnswered').length==4){
+			q2Appear();
+		}
+		else if($('.unAnswered').length==3){
+			q3Appear();
+		}
+		else if($('.unAnswered').length==2){
+			q4Appear();
+		}
+		else if($('.unAnswered').length==1){
+			q5Appear();
+		}
+		else{
+			$('body').css('background-image','url(Images/Universe.jpg)');
+			$('.finalResult').append(message);
+			$('#restart').show();
+			$('#fact5').hide();
+			$('#next_button_holder').hide();
+		}
 	});
 
 //  start the quiz
 
-	$('#start').keydown(function(){
+/*	$('.questionsList').on("click","#start",function(){
 		currentQuestion=0;
 		totalCorrect=0;
-		var newQuestion="<div class="questionsList">"+questions[currentQuestion].question+"</div><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="0">"+questions[currentQuestion].options[0]+"</div></span><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="1">"+questions[currentQuestion].options[1]+"</div></span><br /><div id="button_holder"><input type="button" id="submit" value="Submit"></div>";
+		var newQuestion='<div class="questionsList">'+questions[currentQuestion].question+'</div><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="0">'+questions[currentQuestion].options[0]+'</div></span><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="1">'+questions[currentQuestion].options[1]+'</div></span><br /><div id="button_holder"><input type="button" id="submit" value="Submit"></div>';
 		$(".questionsList").html(newQuestion);
 		$(".feedback_holder").html("");
 	});
 
+*/
+
 //  update to new question
 
-	function updateQuestion(){
+/*	function updateQuestion(){
 		var ans=$("input[type='radio']:checked").val();
 		if(ans==questions[currentQuestion].answer){
 			totalCorrect++;
 		}
 	}
 
+*/
+
 //  feedback appears
 
-	function feedback(){
-		var explanation="<span class="facts"><div class="feedback_holder">"+questions[currentQuestion].description+"</span></div><div id="button_holder"><input type="button" id="next" value="Next"></div>";
+/*	function feedback(){
+		var explanation='<span class="facts"><div class="feedback_holder">'+questions[currentQuestion].description+'</span></div><div id="button_holder"><input type="button" id="next" value="Next"></div>';
 		$('.feedback_holder').html(explanation);
 		nextQuestion();
 	}
 
+*/
+
 //  next question is displayed
 
-	function nextQuestion(){
+/*	function nextQuestion(){
 		if(currentQuestion<5){
 			$('.questionsList').remove();
 			$('.option input').remove();
 			$('.option span').remove();
 			$('.feedback_holder').remove();
-			var newQuestion="<div class="questionsList">"+questions[currentQuestion].question+"</div><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="0">"+questions[currentQuestion].options[0]+"</div></span><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="1">"+questions[currentQuestion].options[1]+"</div></span><br /><div id="button_holder"><input type="button" id="submit" value="Submit"></div>";
+			var newQuestion='<div class="questionsList">'+questions[currentQuestion].question+'</div><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="0">'+questions[currentQuestion].options[0]+'</div></span><br /><span class="answers"><div id="answer_holder"><input type="radio" name="option" class="option" value="1">'+questions[currentQuestion].options[1]+'</div></span><br /><div id="button_holder"><input type="button" id="submit" value="Submit"></div>';
 			$('.questionList').html(newQuestion);
 		}
 		else{
@@ -107,14 +359,14 @@ $(document).ready(function(){
 			$('#next').css("display","none");
 			$('#restart').css("display","inline");
 			if(totalCorrect==1){
-				var finalScore="<span id="final">You got "+totalCorrect+" question correct!</span>";
+				var finalScore='<span id="final">You got ' +totalCorrect+ ' question correct!</span>';
 				$('.answers').html(finalScore);
 			}
 			else{
-				var finalScore="<span id="final">You got "+totalCorrect+" questions correct!</span>";
+				var finalScore='<span id="final">You got ' +totalCorrect+ ' questions correct!</span>';
 				$('.answers').html(finalScore);
 			}
 		}
 	}
-
-})
+*/
+});
